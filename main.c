5a774c5f021c4f1775a96fc76659d63bb2f74fbe
livia-lima <liivia.lima13@gmail.com>
2026-06-06 03:30:40 -0300

    att organização do esqueleto do projeto + add funções e structs de gerenciamento de bairros

--- main.c
+++ main.c
@@ -10,11 +10,31 @@
 // ==========================================
 // STRUCTS
 // ==========================================
+//NÓ BAIRRO
+typedef struct bairro{ //NÓ
+    int codigo;
+    char nome[50];
+    Sensor *listaSensores; //vai apontar para uma estrutura S->begin e S->end
+    struct bairro *prox;
+}Bairro;
+
+//NÓ CABEÇA BAIRRO
+typedef struct _listaBairros{
+    Bairro *inicio;
+    Bairro *final;
+}listaBairros;
+
 
 // ==========================================
 // GERENCIAMENTO DE BAIRROS
 // ==========================================
+listaBairros *criarListaBairros();
+Bairro *criarBairro();
+Bairro *buscarBairro (int codigo, listaBairros *B);
 
+void cadastrarBairro (int codigo, char *nome, listaBairros *B);
+void listarBairros (listaBairros *B);
+void removerBairro (listaBairros *B, int codigo);
 // ==========================================
 // GERENCIAMENTO DE SENSORES
 // ==========================================
@@ -33,4 +53,140 @@ int main()
      listaBairros *B = criarBairro();
      */
     return 0;
+}
+
+// ==========================================
+// GERENCIAMENTO DE BAIRROS
+// ==========================================
+listaBairros *criarListaBairros()
+{
+    listaBairros *B = (listaBairros *) calloc(1, sizeof(listaBairros));
+
+    return B;
+}
+
+Bairro *criarBairro()
+{
+    Bairro *b = (Bairro *) calloc(1, sizeof(Bairro));
+
+    return b;
+}
+
+Bairro *buscarBairro (int codigo, listaBairros *B)
+{
+    Bairro *navegador = B->inicio;
+
+    while (navegador != NULL && navegador->codigo != codigo)
+    {
+        navegador = navegador->prox;
+    }
+
+    return navegador;  
+}
+
+void cadastrarBairro (int codigo, char *nome, listaBairros *B) //equivalente a inserção. inserção no final
+{
+    //verificar se já não existe um bairro com o código digitado
+    Bairro *verificacao = buscarBairro (codigo, B);
+
+    if (verificacao != NULL)
+    {
+        printf("Bairro já existente!\n");
+        return;
+    }
+    else
+    {
+        //alocação do espaço + registro das informações
+        Bairro *b = criarBairro();
+        b->codigo = codigo;
+        strcpy(b->nome, nome); //melhorar? jeito mais eficiente?
+        b->listaSensores = criarListaSensor();
+
+        //ONDE de fato alocar
+        if (B->inicio == NULL && B->final == NULL)
+        {
+            B->inicio = b;
+            B->final = b;
+        }
+        else
+        {
+            B->final->prox = b;
+            B->final = b;
+        }
+    }
+
+    verificacao = NULL; //limpeza da memória
+}
+
+void listarBairros (listaBairros *B)
+{
+    Bairro *navegador = B->inicio;
+
+    if (B->inicio != NULL && B->final != NULL)
+    {
+        while (navegador != NULL)
+        {
+            printf("Nome do bairro: %s\nCódigo cadastrado: %d\n", navegador->nome, navegador->codigo);
+            navegador = navegador->prox;
+        }
+    }
+    else
+        printf("Não há bairros cadastrados!\n");
+    
+    navegador = NULL;
+}
+
+void removerBairro (listaBairros *B, int codigo)
+{
+    //percorrem a lista
+    Bairro *navegador = B->inicio;
+    Bairro *aux = NULL;
+
+    //procuram o bairro a ser removido
+    if (B->inicio != NULL && B->final != NULL)
+    {
+        while (navegador != NULL && navegador->codigo != codigo)
+        {
+            aux = navegador;
+            navegador = navegador->prox;
+
+        }
+    }
+
+    //análise do endereço de memória encontrado (para onde o navegador aponta)
+    if (navegador == NULL)
+    {
+        printf("Erro: bairro não encontrado.\n");
+        return;
+    }
+    if (navegador->listaSensores != NULL)
+    {
+        printf("Erro: não é possível remover bairro com sensores cadastrados.\n");
+        return;
+    }
+    //CASO 01: É o único elemento da lista
+    if (B->inicio == B->final)
+    {
+        B->inicio = NULL;
+        B->final = NULL;
+    }
+    //CASO 02: primeiro elemento da lista
+    else if (navegador == B->inicio)
+    {
+        B->inicio = navegador->prox;
+    }
+    //CASO 03: ultimo elemento da lista
+    else if (navegador == B->final)
+    {
+        aux->prox = NULL;
+        B->final = aux;
+    }
+    else
+    {
+        aux->prox = navegador->prox;
+    }
+
+    free(navegador);
+    navegador = NULL;
+    aux = NULL;
 }
